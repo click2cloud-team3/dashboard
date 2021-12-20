92fbfe20bf0c68409c6e7c80f9e063920cc0aacc
shreyac2c <shreya.dubey@click2cloud.net>
2021-12-20 10:57:05 +0530

    code for role creation after removing console.

--- src/app/frontend/common/dialogs/createRole/dialog.ts
+++ src/app/frontend/common/dialogs/createRole/dialog.ts
@@ -1,19 +1,3 @@
-// Copyright 2020 Authors of Arktos - file modified.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
-
 import {Component, OnInit, Inject} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
@@ -121,7 +105,6 @@ export class CreateRoleDialog implements OnInit {
     this.verbs1 = this.verbs.value.split(',')
 
     const tenantSpec= {name: this.role.value, namespace: this.namespace.value, apiGroups: this.apigroups1,verbs: this.verbs1,resources: this.resources1};
-    console.log(tenantSpec)
     const tokenPromise = this.csrfToken_.getTokenForAction('role');
     tokenPromise.subscribe(csrfToken => {
       return this.http_
@@ -135,7 +118,6 @@ export class CreateRoleDialog implements OnInit {
         .subscribe(
           () => {
             this.dialogRef.close(this.role.value);
-            console.log("role created ")
           },
           error => {
             this.dialogRef.close();
